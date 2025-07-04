ba15030fd8bcb0a6aa93b819a65907e03964456e
courage <courageagbenyegah234@gmail.com>
2025-07-04 09:20:38 +0000

    fix ui on login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,12 +10,17 @@
 	}
 </script>
 
-<div class="flex h-1/2 w-5/6 flex-col items-center justify-center gap-5 bg-[#f2ece3]">
+<div
+	class="flex h-1/2 w-5/6 flex-col items-center justify-center gap-5 bg-[#f2ece3] md:h-[400px] md:w-[400px]"
+>
 	<div class="flex w-full justify-center">
 		<img alt="logo" class="mr-5 w-[60px] rounded-sm" src="logo.png" />
 	</div>
 
-	<form on:submit|preventDefault={handleSubmit} class="h-1/2 w-4/5 rounded bg-white p-4 shadow">
+	<form
+		on:submit|preventDefault={handleSubmit}
+		class="h-1/2 w-4/5 rounded bg-white p-4 shadow md:h-[250px]"
+	>
 		<div class="mb-4">
 			<label class="block text-sm font-medium text-gray-700">Email</label>
 			<input
