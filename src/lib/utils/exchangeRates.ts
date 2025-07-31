1ba260b942e57dce6cd3e1d21b5e6a126f5589a6
courage <courageagbenyegah234@gmail.com>
2025-07-31 07:21:01 +0000

    fixed shipping rate problem by changing rating

--- src/lib/utils/exchangeRates.ts
+++ src/lib/utils/exchangeRates.ts
@@ -1,6 +1,8 @@
-export default {
-	shillion: 25611, // Assuming 1 USD = 25611 TSh
+const exchangeRates = {
+	shillion: 2570, // Assuming 1 USD = 2570 TSh
 	convertToTSh: (amountInUSD: number) => {
-		return Math.round(amountInUSD * 25611 ) / 100;
-	}
+		return Math.round(amountInUSD * exchangeRates.shillion * 100) / 100;
+	},
 };
+
+export default exchangeRates;
